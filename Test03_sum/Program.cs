e51b5f24f0ed014f9019371c2c02d4603a797f9a
Renat <remch2013ya.ru>
2023-09-19 21:05:58 +0400

    Создал тест 3 random

--- Test03_sum/Program.cs
+++ Test03_sum/Program.cs
@@ -1,5 +1,6 @@
-﻿int A = 35;
-int B = 7;
-int result = A + B;
+﻿int numA = new Random().Next(1, 10); // 1 2 3 4...10
+Console.WriteLine(numA);
+int numB = new Random().Next(1, 10);
+Console.WriteLine(numB);
+int result = numA + numB;
 Console.WriteLine(result);
-
