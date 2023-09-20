602490f6858d4f3a1f6a665b1722b3910470917e
Renat <remch2013ya.ru>
2023-09-20 21:59:53 +0400

    создал тест масив2

---
+++ Test09_masive2/Program.cs
@@ -0,0 +1,26 @@
+﻿void FillArray(int[] collection)
+{
+    int lengtn = collection.Length;
+    int index = 0;
+    while (index < lengtn)
+    {
+        collection[index] = new Random().Next(0, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] Array = new int[10];
+
+FillArray(Array);
+PrintArray(Array);
