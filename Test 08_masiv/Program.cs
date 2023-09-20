3b526b7aab6431e9a488dd9df0869e9ebbf67ae9
Renat <remch2013ya.ru>
2023-09-20 21:54:20 +0400

    создал тест масив2

--- Test 08_masiv/Program.cs
+++ Test 08_masiv/Program.cs
@@ -1,19 +1,19 @@
-﻿using System; 
-int [] Array = {12,2,3,4,3,52,6,71,3};
+﻿using System;
+int[] Array = { 12, 2, 3, 4, 3, 52, 6, 71, 3 };
 
 int n = Array.Length;
 int find = 3;
 
 int index = 0;
 
-while (index < n) 
+while (index < n)
 {
     if (Array[index] == find)
     {
-        Console.WriteLine(index);  
+        Console.WriteLine(index);
         break;
-    } 
-       
-       index++;
+    }
+
+    index++;
 
 }
